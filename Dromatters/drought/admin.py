691c614601061085bc5dec6e17b205dedc61eef7
slr1997 <331966609@qq.com>
2017-08-04 16:06:46 +0800

    Update admin.py

--- Dromatters/drought/admin.py
+++ Dromatters/drought/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from drought import models
+admin.site.register(models.beijing)
+admin.site.register(models.tianjin)
+admin.site.register(models.hebei)
+admin.site.register(models.shanxi)
+admin.site.register(models.neimenggu)
+admin.site.register(models.liaoning)
+admin.site.register(models.jilin)
+admin.site.register(models.heilongjiang)
+admin.site.register(models.jiangsu)
+admin.site.register(models.anhui)
+admin.site.register(models.shandong)
+admin.site.register(models.henan)
+admin.site.register(models.shaanxi)
+admin.site.register(models.gansu)
+admin.site.register(models.qinghai)
+admin.site.register(models.ningxia)
+admin.site.register(models.xinjiang)
