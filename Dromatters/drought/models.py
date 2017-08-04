ccb58e009dee344e56a83e804ed6585c3b3c1333
slr1997 <331966609@qq.com>
2017-08-04 16:06:05 +0800

    Update models.py

--- Dromatters/drought/models.py
+++ Dromatters/drought/models.py
@@ -1,3 +1,172 @@
 from django.db import models
 
 # Create your models here.
+class beijing(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class tianjin(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class hebei(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class shanxi(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class neimenggu(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class liaoning(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class jilin(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class heilongjiang(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class jiangsu(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class anhui(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class shandong(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class henan(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class shaanxi(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class gansu(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class qinghai(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class ningxia(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
+
+class xinjiang(models.Model):
+    index = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=20,null=True)
+    stationIndex = models.IntegerField(null=True)
+    Year = models.IntegerField(null=True)
+    Area = models.FloatField(null=True)
+    Precipitation = models.IntegerField(null=True)
+    totalPre = models.FloatField(null=True)
+    Comparing = models.FloatField(null=True)
