20ce9ee06bb1c6ed509960d80d8053847ef3e21e
Mrzsw666 <2074638138@qq.com>
2017-08-11 15:01:33 +0800

    models update

--- Dromatters/drought/models.py
+++ Dromatters/drought/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class beijing(models.Model):
+
+
+class RFBeijing(models.Model):    #北京
     index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
+    cityName = models.CharField(max_length=20, null=True)
     stationIndex = models.IntegerField(null=True)
     Year = models.IntegerField(null=True)
     Area = models.FloatField(null=True)
@@ -11,162 +13,66 @@ class beijing(models.Model):
     totalPre = models.FloatField(null=True)
     Comparing = models.FloatField(null=True)
 
-class tianjin(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
 
-class hebei(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
+class RFTianjin(models.Model):    #天津
+    pass
 
-class shanxi(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
 
-class neimenggu(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
+class RFHebei(models.Model):      #河北
+    pass
 
-class liaoning(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
 
-class jilin(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
+class RFShanxi(models.Model):     #山西
+    pass
 
-class heilongjiang(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
 
-class jiangsu(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
+class RFNeimenggu(models.Model):  #内蒙古
+    pass
 
-class anhui(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
 
-class shandong(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
+class RFLiaoning(models.Model):   #辽宁
+    pass
 
-class henan(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
 
-class shaanxi(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
+class RFJilin(models.Model):      #吉林
+    pass
 
-class gansu(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
 
-class qinghai(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
+class RFHeilongjiang(models.Model): #黑龙江
+    pass
 
-class ningxia(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
 
-class xinjiang(models.Model):
-    index = models.AutoField(primary_key=True)
-    cityName = models.CharField(max_length=20,null=True)
-    stationIndex = models.IntegerField(null=True)
-    Year = models.IntegerField(null=True)
-    Area = models.FloatField(null=True)
-    Precipitation = models.IntegerField(null=True)
-    totalPre = models.FloatField(null=True)
-    Comparing = models.FloatField(null=True)
+class RFJiangsu(models.Model):     #江苏
+    pass
+
+
+class RFAnhui(models.Model):      #安徽
+    pass
+
+
+class RFShandong(models.Model):   #山东
+    pass
+
+
+class RFHenan(models.Model):      #河南
+    pass
+
+
+class RFShaanxi(models.Model):    #陕西
+    pass
+
+
+class RFGansu(models.Model):      #甘肃
+    pass
+
+
+class RFQinghai(models.Model):    #青海
+    pass
+
+
+class RFNingxia(models.Model):    #宁夏
+    pass
+
+
+class RFXinjiang(models.Model):   #新疆
+    pass
