ad9769c842b3d0d156d475ac18e0908f43508d31
Mrzsw666 <2074638138@qq.com>
2017-09-03 14:22:38 +0800

    fix data

--- Dromatters/urls.py
+++ Dromatters/urls.py
@@ -17,8 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from drought.models import RFBeijing, RFTianjin, RFHebei, RFShanxi, RFNeimenggu, RFLiaoning, RFJilin, RFHeilongjiang, \
     RFJiangsu, RFAnhui, RFShandong, RFHenan, RFShaanxi, RFGansu, RFQinghai, RFNingxia, RFXinjiang
-from drought.views import data
-from drought.views import CITYS_RFDB, CITYS_CNS
+from drought.views import data, redi, region, knowledge
 import re
 import requests
 import os
@@ -26,7 +25,10 @@ import django
 from threading import Timer
 
 urlpatterns = [
+    url(r'^$', redi, name="webroot"),
     url(r'^data/$', data, name="data"),
+    url(r'region/$', region, name="region"),
+    url(r'knowledge/', knowledge, name="knowledge"),
     url(r'^admin/', admin.site.urls),
 ]
 # os.environ.update({"DJANGO_SETTINGS_MODULE": "Dromatters.settings"})
@@ -37,210 +39,22 @@ django.setup()
 pages = ['1', '2', '3', '4', '5', '6', '7']
 init_url = "http://www.data.ac.cn/zrzy/ntBA02.asp"
 
-'''
-def get_info():
-    for cur_page in pages:
-        payload = {'Page': cur_page}
-        r = requests.get(init_url, params=payload)
-        r.encoding = 'GBK'
-        p1 = r"(?<=<TD>).+?(?=</TD>)"
-        pattern = re.compile(p1)
-        result = re.findall(pattern, r.text)
-        a = 0
-        for key in result:
-            if a % 10 == 0:
-                if result[a + 1] == "北京      ":
-                    RFBeijing.objects.create(stationIndex=0)
-                    obj = RFBeijing.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "天津      ":
-                    RFTianjin.objects.create(stationIndex=0)
-                    obj = RFTianjin.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "河北      ":
-                    RFHebei.objects.create(stationIndex=0)
-                    obj = RFHebei.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "山西      ":
-                    RFShanxi.objects.create(stationIndex=0)
-                    obj = RFShanxi.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "内蒙古    ":
-                    RFNeimenggu.objects.create(stationIndex=0)
-                    obj = RFNeimenggu.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "辽宁      ":
-                    RFLiaoning.objects.create(stationIndex=0)
-                    obj = RFLiaoning.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "吉林      ":
-                    RFJilin.objects.create(stationIndex=0)
-                    obj = RFJilin.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "黑龙江    ":
-                    RFHeilongjiang.objects.create(stationIndex=0)
-                    obj = RFHeilongjiang.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "江苏      ":
-                    RFJiangsu.objects.create(stationIndex=0)
-                    obj = RFJiangsu.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "安徽      ":
-                    RFAnhui.objects.create(stationIndex=0)
-                    obj = RFAnhui.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "山东      ":
-                    RFShandong.objects.create(stationIndex=0)
-                    obj = RFShandong.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "河南      ":
-                    RFHenan.objects.create(stationIndex=0)
-                    obj = RFHenan.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "陕西      ":
-                    RFShaanxi.objects.create(stationIndex=0)
-                    obj = RFShaanxi.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "甘肃      ":
-                    RFGansu.objects.create(stationIndex=0)
-                    obj = RFGansu.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "青海      ":
-                    RFQinghai.objects.create(stationIndex=0)
-                    obj = RFQinghai.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "宁夏      ":
-                    RFNingxia.objects.create(stationIndex=0)
-                    obj = RFNingxia.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-                elif result[a + 1] == "新疆      ":
-                    RFXinjiang.objects.create(stationIndex=0)
-                    obj = RFXinjiang.objects.get(stationIndex=0)
-                    obj.cityName = result[a + 1]
-                    obj.stationIndex = int(result[a + 2])
-                    obj.Year = int(result[a + 3])
-                    obj.Area = float(result[a + 4])
-                    obj.Precipitation = int(result[a + 5])
-                    obj.totalPre = float(result[a + 6])
-                    obj.Comparing = float(result[a + 8])
-                    obj.save()
-            a += 1'''
+CITYS_RFDB = {u'Beijing': RFBeijing, u'Tianjin': RFTianjin, u'Hebei': RFHebei, u'Shanxi': RFShanxi,
+              u'Neimenggu': RFNeimenggu, u'Liaoning': RFLiaoning, u'Jilin': RFJilin, u'Heilongjiang': RFHeilongjiang,
+              u'Jiangsu': RFJiangsu, u'Anhui': RFAnhui, u'Shandong': RFShandong, u'Henan': RFHenan,
+              u'Shaanxi': RFShaanxi, u'Gansu': RFGansu, u'Qinghai': RFQinghai, u'Ningxia': RFNingxia,
+              u'Xinjiang': RFXinjiang}
+
+CITYS_CNS = {u'Beijing': "北京      ", u'Tianjin': "天津      ", u'Hebei': "河北      ", u'Shanxi': "山西      ",
+             u'Neimenggu': "内蒙古    ", u'Liaoning': "辽宁      ", u'Jilin': "吉林      ", u'Heilongjiang': "黑龙江    ",
+             u'Jiangsu': "江苏      ", u'Anhui': "安徽      ", u'Shandong': "山东      ", u'Henan': "河南      ",
+             u'Shaanxi': "陕西      ", u'Gansu': "甘肃      ", u'Qinghai': "青海      ", u'Ningxia': "宁夏      ",
+             u'Xinjiang': "新疆      "}
 
 
 def get_info():
     a = 0
+    cnt = 0
     for cur_page in pages:
         payload = {'Page': cur_page}
         r = requests.get(init_url, params=payload)
@@ -248,10 +62,14 @@ def get_info():
         p1 = r"(?<=<TD>).+?(?=</TD>)"
         pattern = re.compile(p1)
         result = re.findall(pattern, r.text)
-        print(result)
+        # print(result)
         while a != 1340:
             for key in CITYS_CNS:
                 if CITYS_CNS[key] == result[a % 200 + 1]:
+                    y = int(result[a % 200 + 3])
+                    qset = CITYS_RFDB[key].objects.filter(Year=y)
+                    if qset:
+                        continue
                     CITYS_RFDB[key].objects.create(stationIndex=0)
                     obj = CITYS_RFDB[key].objects.get(stationIndex=0)
                     obj.cityName = result[a % 200 + 1]
@@ -266,6 +84,31 @@ def get_info():
             a += 10
             if a == 200 or a == 400 or a == 600 or a == 800 or a == 1000 or a == 1200:
                 break
+        print(cnt)
+        cnt += 1
+    fix()
 
+
+def fix():
+    for y, x in CITYS_RFDB.items():
+        obj = x.objects.get(Year=1996)
+        area = obj.Area
+        rain = obj.Precipitation
+        com = 1+(obj.Comparing/100)
+        ave = rain/com
+        obj.save()
+        obj = x.objects.get(Year=1997)
+        obj.Area = area
+        obj.save()
+        obj = x.objects.get(Year=1998)
+        obj.Area = area
+        pre = round((obj.totalPre/area)*10, 1)
+        obj.Precipitation = pre
+        obj.Comparing = round((pre-ave)*100/ave, 1)
+        obj.save()
+        obj = x.objects.get(Year=1999)
+        obj.Area = area
+        obj.Comparing = round((obj.Precipitation-ave)*100/ave, 1)
+        obj.save()
 
 Timer(0, get_info).start()
--- drought/views.py
+++ drought/views.py
@@ -4,37 +4,55 @@ from drought.models import RFBeijing, RFTianjin, RFHebei, RFShanxi, RFNeimenggu,
     RFHeilongjiang, RFJiangsu, RFAnhui, RFShandong, RFHenan, RFShaanxi, RFGansu, RFQinghai, RFNingxia, RFXinjiang
 from django.shortcuts import render
 import tensorflow
-
+from django.http import HttpResponseRedirect, Http404
 
 CITYS_RFDB = {u'Beijing': RFBeijing, u'Tianjin': RFTianjin, u'RFHebei': RFHebei, u'Shanxi': RFShanxi,
               u'Neimenggu': RFNeimenggu, u'Liaoning': RFLiaoning, u'Jilin': RFJilin, u'Heilongjiang': RFHeilongjiang,
-              u'Jiangsu': RFJiangsu, u'Anhui': RFAnhui, u'Shandong': RFShandong, u'Henan': RFHenan, u'Shaanxi': RFShaanxi,
-              u'Gansu': RFGansu, u'Qinghai': RFQinghai, u'Ningxia':RFNingxia, u'Xinjiang': RFXinjiang}
+              u'Jiangsu': RFJiangsu, u'Anhui': RFAnhui, u'Shandong': RFShandong, u'Henan': RFHenan,
+              u'Shaanxi': RFShaanxi,
+              u'Gansu': RFGansu, u'Qinghai': RFQinghai, u'Ningxia': RFNingxia, u'Xinjiang': RFXinjiang}
 
 # CITYS_CN = {'北京', '天津', '河北', '山西', '内蒙古', '辽宁', '吉林', '黑龙江', '江苏', '安徽', '山东', '河南', '陕西', '甘肃', '青海', '宁夏', '新疆'}
 
-CITYS_CNS = {u'Beijing': "北京      ", u'Tianjin': "天津      ", u'RFHebei': "河北      ", u'Shanxi': "山西      ",
-            u'Neimenggu': "内蒙古    ", u'Liaoning': "辽宁      ", u'Jilin': "吉林      ", u'Heilongjiang':
-            "黑龙江    ",u'Jiangsu': "江苏      ", u'Anhui': "安徽      ", u'Shandong':
-            "山东      ", u'Henan': "天津      ", u'Shaanxi': "陕西      ",u'Gansu': "甘肃      ", u'Qinghai':
-            "青海      ", u'Ningxia':"宁夏      ", u'Xinjiang': "新疆      "}
+CITYS_CNS = {u'Beijing': "北京", u'Tianjin': "天津", u'Hebei': "河北", u'Shanxi': "山西", u'Neimenggu': "内蒙古",
+             u'Liaoning': "辽宁", u'Jilin': "吉林", u'Heilongjiang':"黑龙江", u'Jiangsu': "江苏", u'Anhui': "安徽",
+             u'Shandong':"山东", u'Henan': "天津", u'Shaanxi': "陕西", u'Gansu': "甘肃", u'Qinghai':"青海",
+             u'Ningxia': "宁夏", u'Xinjiang': "新疆"}
+
+
+def redi(request):
+    return HttpResponseRedirect('/data')
+
 
 def data(request):
     llist = []
     dlist = []
     try:
-        for i in (1995, 1999):
-            for x in CITYS_RFDB:
-                city_note = CITYS_RFDB[x]
-                qset = city_note.objects.all()
-                for v in qset:
-                    llist.append(x)
-                    dlist.append(v.Year)
-                    dlist.append(v.Area)
-                    dlist.append(v.Precipitation)
-                    dlist.append(v.totalPre)
-                    dlist.append(v.Comparing)
-        return render(request, 'data.html', {'llist': llist, 'dlist':dlist})
+        for x, y in CITYS_RFDB.items():
+            qset = y.objects.all().order_by('Year')
+            city_name = CITYS_CNS[x]
+            llist.append(city_name)
+            for v in qset:
+                dlist.append(v)
+        return render(request, 'data.html', {'statu': u"OK", 'llist': llist, 'dlist': dlist})
     except Exception as e:
         print(e)
-        return render(request, 'data.html')
+        return render(request, 'data.html', {'statu': u"ERROR"})
+
+
+def region(request):
+    city_name = request.GET.get('city')
+    obj = CITYS_RFDB[city_name]
+    llist = []
+    try:
+        qset = obj.objects.all().order_by('Year')
+        for v in qset:
+            llist.append(v)
+        return render(request, 'region.html', {'statu': u"OK", 'llist': llist})
+    except Exception as e:
+        print(e)
+        return render(request, 'region.html', {'statu': u"ERROR"})
+
+
+def knowledge(request):
+    return render(request, 'knowledge.html', {'statu': "OK"})
