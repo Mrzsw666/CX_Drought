f5ff7613159757e72040ff3b4a50efa7d081dfb3
Liz-Wang <763394591@qq.com>
2018-01-25 22:04:19 +0800

    增加降水数据的折线图，增加折叠等局部美化

--- static/js/region.js
+++ static/js/region.js
@@ -33,6 +33,40 @@ function ParametersFunc() {
         }
         $("#tianqi").html(item);
 
+
+
     })
 }
 ParametersFunc();
+//折线图
+$(function () {
+            var test = $.ajax({
+                type: "GET",
+                url: "/RegionData.json",//利用ajax请求后台的并返回值
+                dataType: "json",
+                data: {cityName: city,year: year},
+                async: false
+            });
+            var data1 = [];
+            var data2 = [];
+            data1 = test.responseJSON.results;
+            for(var i = 0; i < data1.length; i++) {
+                data2[i] = data1[i].rainfall;
+            }
+            var lineChartData = {
+                labels : ["January","February","March","April","May","June","July","August","September","October","November","December"],
+                datasets : [
+                    {
+                        fillColor : "rgba(151,187,205,0.5)",
+                        strokeColor : "rgba(151,187,205,1)",
+                        pointColor : "rgba(151,187,205,1)",
+                        pointStrokeColor : "#fff",
+                        data : [data2[0],data2[1],data2[2],data2[3],data2[4],data2[5],data2[6],data2[7],data2[8],data2[9],data2[10],data2[11]]
+                    }
+                ]
+
+            }
+
+            var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
+
+})
